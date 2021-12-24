41390305960214555c7a02797d750dddec896924
Pavel <slivnitsin1997@mail.ru>
2021-12-24 18:22:48 +0500

    Create bibl2.py

---
+++ bibl2.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+SERVER_IP = 'https://api-yofactory.dats.team/'
+ID = 'https://yofactory.dats.team/?id=01FQP5MPMP7QPKANH2FZQJNV0Z'
+KEY = 'a524580d-1344-4efb-a35e-e07c9d5b0ec2'
+
+def ask(url, data):
+  response = requests.post(url=url, json=data)
+  out = response.content
+  return out
+
+
+print(ask(ID, {'key' : KEY}))

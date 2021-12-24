b6f27b189861230fc31daf8ba53dc2ed1de201b1
Alexandr <knxandr@rambler.ru>
2021-12-24 18:16:17 +0500

    Update bibl.py

--- bibl.py
+++ bibl.py
@@ -6,9 +6,13 @@ ID = 'https://yofactory.dats.team/?id=01FQP5MPMP7QPKANH2FZQJNV0Z'
 KEY = 'a524580d-1344-4efb-a35e-e07c9d5b0ec2'
 
 def ask(url, data):
-  response = requests.post(url=url, json=data)
+  response = requests.post(url=url, params=data)
   out = response.content
   return out
 
 
-print(ask(ID, {'key' : KEY}))
+#print(ask(ID, {'key' : KEY}))
+result = requests.get(ID,
+                      headers={'authorization' : KEY})
+
+print(result.content)

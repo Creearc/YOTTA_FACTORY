14f8adbb01ebadd4b214b1c909347c4638bf3801
Alexandr <knxandr@rambler.ru>
2021-12-24 16:50:18 +0500

    Nikita

--- bibl.py
+++ bibl.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 SERVER_IP = 'http://127.0.0.1:8081'
+KEY = ''
 
 def ask(url, question_text):
   url = '{}/question'.format(url)
@@ -9,12 +10,3 @@ def ask(url, question_text):
   response = requests.post(url=url, json=data)
   out = response.json()
   return out
-
-def ask2(question_text, result_type, component):
-  response = requests.post(url=qanary_pipeline_url,
-                             params={
-                                 "question": question_text,
-                                 "componentlist[]": component
-                             }).json()
-  result = get_final_result(result_type, response['endpoint'], response['inGraph'])
-  return result

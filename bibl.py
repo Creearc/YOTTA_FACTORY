cbdddd24c05685e1b1ccc0bad529e39b4b100619
Alexandr <knxandr@rambler.ru>
2021-12-24 17:49:39 +0500

    Update bibl.py

--- bibl.py
+++ bibl.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 SERVER_IP = 'http://127.0.0.1:8081'
-KEY = ''
+KEY = 'a524580d-1344-4efb-a35e-e07c9d5b0ec2'
 
 def ask(url, question_text):
   url = '{}/question'.format(url)

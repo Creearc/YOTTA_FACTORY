689c565528669b91d1094a281c2552c1a125260a
Alexandr <knxandr@rambler.ru>
2021-12-24 18:09:43 +0500

    Update bibl.py

--- bibl.py
+++ bibl.py
@@ -5,9 +5,10 @@ SERVER_IP = 'https://api-yofactory.dats.team/'
 ID = 'https://yofactory.dats.team/?id=01FQP5MPMP7QPKANH2FZQJNV0Z'
 KEY = 'a524580d-1344-4efb-a35e-e07c9d5b0ec2'
 
-def ask(url, question_text):
-  url = '{}/question'.format(url)
-  data = {'question' : question_text}
+def ask(url, data):
   response = requests.post(url=url, json=data)
-  out = response.json()
+  out = response.content
   return out
+
+
+print(ask(ID, {'key' : KEY}))

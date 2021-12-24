49ebef90f807e89a4d74fd523bcf534e0c29c8c7
Alexandr <knxandr@rambler.ru>
2021-12-24 19:01:55 +0500

    Update bibl.py

--- bibl.py
+++ bibl.py
@@ -1,22 +1,35 @@
 import requests
 import json
+import pprint
 
 BASE_URL = 'api-yofactory.dats.team'
 ID = '01FQP5MPMP7QPKANH2FZQJNV0Z'
 KEY = 'a524580d-1344-4efb-a35e-e07c9d5b0ec2'
 
-def ask(url, data):
-  response = requests.post(url=url, params=data)
-  out = response.content
-  return out
+def scan_map():
+  result = requests.get('https://{}/factory/{}/map'.format(BASE_URL, ID),
+                        headers={'authorization' : KEY})
 
+  return result.json()
 
-##result = requests.get('https://{}/factory/{}/map'.format(BASE_URL, ID),
-##                      headers={'authorization' : KEY})
-##
-##print(result.json())
-
-result = requests.get('https://{}/netscan'.format(BASE_URL),
+def devices():
+  result = requests.get('https://{}/devices'.format(BASE_URL),
                       headers={'authorization' : KEY})
 
-print(result.json())
+  return result.json()
+
+def move(x=57, y=135, idd=ee7f:4e3f:8236:67d4:fe08:af2:6acf:ebaf'):
+  result = requests.post( url='https://{}/devices/{}'.format(BASE_URL, drone_id),
+                        headers={'authorization' : KEY,
+                                 'content-type' : 'application/json'},
+                        data = {"actionType": "move",
+                                "x": x,
+                                "y": y})
+  return result.json()
+
+
+
+
+pprint.pprint()
+
+

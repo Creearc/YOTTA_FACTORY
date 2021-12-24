a5263284b3ded362fdde1bf16bf4f4f6f46df0de
Alexandr <knxandr@rambler.ru>
2021-12-24 18:04:00 +0500

    Update bibl.py

--- bibl.py
+++ bibl.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-SERVER_IP = 'http://127.0.0.1:8081'
+SERVER_IP = 'https://api-yofactory.dats.team/'
+ID = 'https://yofactory.dats.team/?id=01FQP5MPMP7QPKANH2FZQJNV0Z'
 KEY = 'a524580d-1344-4efb-a35e-e07c9d5b0ec2'
 
 def ask(url, question_text):

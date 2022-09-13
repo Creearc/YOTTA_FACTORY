88892fe2b656afe8101e1aaa82596ea2646160c8
Pavel <slivnitsin1997@mail.ru>
2022-09-13 19:16:11 +0500

    Create sort_word.py

---
+++ sort_word.py
@@ -0,0 +1,20 @@
+import json
+from pprint import pprint
+
+
+def get_min_len_word(string_list):
+    return sorted(string_list, key=len)
+
+
+with open(r"data.json", "r", encoding='utf-8') as f:
+    data = json.load(f)
+
+
+words = []
+
+for key in data.keys():
+    words.append(key)
+
+sort_word = sorted(words, key=len)
+
+pprint(sort_word)

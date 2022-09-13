98dd0b9797be0538bcb02a84cafa682cb93f4fe8
Alexandr <knxandr@rambler.ru>
2022-09-13 20:55:20 +0500

    Update word.py

--- word.py
+++ word.py
@@ -36,41 +36,72 @@ data = []
 ##    data.append(tmp)
 
 
-word = 'притоносодержательница'.upper()
-for i in range(len(word)):
-    tmp = {"name" : word[i], "x" : 0, "y" : 0, "z" : len(word) - i - 1}
-    data.append(tmp)
+##word = 'притоносодержательница'.upper()
+##for i in range(len(word)):
+##    tmp = {"name" : word[i], "x" : 0, "y" : 0, "z" : len(word) - i - 1}
+##    data.append(tmp)
+##
+##
+##word = 'трансформаторостроение'.upper()
+##for i in range(len(word)):
+##    tmp = {"name" : word[i], "x" : 2, "y" : 0, "z" : len(word) - i - 1}
+##    data.append(tmp)
+##
+##word = 'пот'.upper()
+##for i in range(len(word)):
+##    tmp = {"name" : word[i], "x" : len(word) - i - 1, "y" : 0, "z" : 21}
+##    data.append(tmp)
+##    #print(tmp)
+##
+##word = 'человеконенавистничество'.upper()
+##for i in range(len(word)):
+##    tmp = {"name" : word[i], "x" : 1, "y" : 0, "z" : 21 + len(word) - i - 1}
+##    data.append(tmp)
+##    #print(tmp)
+##
+###print(data)
 
 
-word = 'трансформаторостроение'.upper()
+word = 'катер'.upper()
 for i in range(len(word)):
-    tmp = {"name" : word[i], "x" : 2, "y" : 0, "z" : len(word) - i - 1}
-    data.append(tmp)
-
-word = 'пот'.upper()
-for i in range(len(word)):
-    tmp = {"name" : word[i], "x" : len(word) - i - 1, "y" : 0, "z" : 21}
+    tmp = {"name" : word[i], "x" : len(word) - i - 1, "y" : 1, "z" : 19}
     data.append(tmp)
     #print(tmp)
 
-word = 'человеконенавистничество'.upper()
+word = 'капер'.upper()
 for i in range(len(word)):
-    tmp = {"name" : word[i], "x" : 1, "y" : 0, "z" : 21 + len(word) - i - 1}
+    tmp = {"name" : word[i], "x" : 1, "y" : len(word) - i - 1, "z" : 19}
     data.append(tmp)
     #print(tmp)
 
-#print(data)
+word = 'ведро'.upper()
+for i in range(len(word)):
+    tmp = {"name" : word[i], "x" : 3, "y" : len(word) - i - 1, "z" : 19}
+    data.append(tmp)
+    #print(tmp)
 
+word = 'дебри'.upper()
+for i in range(len(word)):
+    tmp = {"name" : word[i], "x" : len(word) - i - 1, "y" : 3, "z" : 19}
+    data.append(tmp)
+    #print(tmp)
 
-data_out = []
-for i in range(len(data)):
-    for j in range(len(data)):
+    
+    
+i = 0
+while i < len(data):
+    j = 0
+    while j < len(data):
         if i == j:
+            j += 1
             continue
-        if data[i] == data[j]:
-            data_out.append(data[i])
-            
-data = data_out
+        if data[i]['x'] == data[j]['x'] and data[i]['y'] == data[j]['y'] and data[i]['z'] == data[j]['z']:
+            data.pop(i)
+            print(i)
+        else:
+            j += 1
+    i += 1
+
 print(data)
 
 

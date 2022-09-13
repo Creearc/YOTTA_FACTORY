e49f643907cb58249c30ae7c5e365caa1548615e
Pavel <slivnitsin1997@mail.ru>
2022-09-13 19:16:53 +0500

    Update sort_word.py

--- sort_word.py
+++ sort_word.py
@@ -5,12 +5,12 @@ from pprint import pprint
 with open(r"data.json", "r", encoding='utf-8') as f:
     data = json.load(f)
 
-
 words = []
 
 for key in data.keys():
     words.append(key)
 
 sort_word = sorted(words, key=len)
+sort_word.reverse()
 
 pprint(sort_word)
